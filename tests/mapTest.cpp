8482f7a7f7039358742672bc29abc4f99fdebd6b
thimovandermeer <thimovandermeer@gmail.com>
2021-06-09 09:15:23 +0200

    Test cases set

--- tests/mapTest.cpp
+++ tests/mapTest.cpp
@@ -7,7 +7,8 @@
 #include "../srcs/map/map.hpp"
 #include <iostream>
 
-namespace ft {
+namespace ft
+{
 	// have to create the overload for equals
 
 
@@ -23,8 +24,206 @@ namespace ft {
 
 		}
 
+		SECTION("Copy")
+		{
+
+		}
+	}
+
+	TEST_CASE("Map: Destructors", "[Map]")
+	{
 
 	}
 
+	TEST_CASE("Map: Operator==", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Iterator begin", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+
+	}
+
+	TEST_CASE("Map: Iterator end", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+
+	}
+
+	TEST_CASE("Map: Iterator rbegin", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+
+	}
+
+	TEST_CASE("Map: Iterator rend", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+
+	}
+
+	TEST_CASE("Map: empty", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: size", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Max size", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Operator[]", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Insert", "[Map]")
+	{
+		SECTION("Insert one")
+		{
+
+		}
+
+		SECTION("Insert with hint")
+		{
+
+		}
+
+		SECTION("Insert Range")
+		{
+
+		}
+	}
+
+	TEST_CASE("Map: Erase", "[Map]")
+	{
+		SECTION("Erase one")
+		{
+
+		}
+
+		SECTION("Erase with hint")
+		{
+
+		}
+
+		SECTION("Erase range")
+		{
+
+		}
+	}
+
+	TEST_CASE("Map: Swap", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Clear", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Key_compare", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Value_compare", "Map")
+	{
+
+	}
+
+	TEST_CASE("Map: Find", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Count", "[Map]")
+	{
+
+	}
+
+	TEST_CASE("Map: Lower_bound", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+	}
+
+	TEST_CASE("Map: Upper_bound", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+	}
+
+	TEST_CASE("Map: Equal_range", "[Map]")
+	{
+		SECTION("Normal")
+		{
+
+		}
+
+		SECTION("Const")
+		{
+
+		}
+	}
+
+	TEST_CASE("Map: Get_allocator", "[Map]")
+	{
+
+	}
 
 }
